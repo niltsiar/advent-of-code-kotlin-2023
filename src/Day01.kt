e6107dbda53e33a47df9e208c4d761b4b13d1b32
Borja Quevedo <b.quevedo@minddistrict.com>
2023-12-01 18:12:48 +0100

    Solution for Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,63 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.findFirstAndLastDigitAndSum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.map { it.extractDigits() }
+            .findFirstAndLastDigitAndSum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
+    val testInputB = readInput("Day01b_test")
+    check(part2(testInputB) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
+}
+
+
+private fun List<String>.findFirstAndLastDigitAndSum(): Int {
+    return sumOf { string ->
+        val digits = string.filter { char -> char.isDigit() }
+
+        val firstDigit = digits.first()
+        val lastDigit = digits.last()
+
+        "$firstDigit$lastDigit".toInt()
+    }
+}
+
+private val numberStringToDigit = mapOf(
+    "one" to "1",
+    "two" to "2",
+    "three" to "3",
+    "four" to "4",
+    "five" to "5",
+    "six" to "6",
+    "seven" to "7",
+    "eight" to "8",
+    "nine" to "9",
+)
+
+private fun String.extractDigits(): String {
+    val line = this
+    return buildString {
+        for (i in line.indices) {
+            if (line[i].isDigit()) {
+                append(line[i])
+            } else {
+                val sub = line.substring(startIndex = i)
+                for ((word, num) in numberStringToDigit) {
+                    if (sub.startsWith(word)) {
+                        append(num)
+                        break
+                    }
+                }
+            }
+        }
+    }
 }
